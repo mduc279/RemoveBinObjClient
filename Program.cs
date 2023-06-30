f1cee1ca9b7020a2d4eaf24e9f5219ebfa16110c
Duck <dmd@aod.vn>
2023-06-30 13:55:56 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿var sourceFolder = @"D:\AOD Projects\DSSClients";
+var directory = Directory.GetDirectories(sourceFolder, "*", SearchOption.AllDirectories);
+var binObjFolders = directory.Where(x => x.EndsWith("bin") || x.EndsWith("obj") || x.EndsWith(@"AOD.DSSClients.UWP\AppPackages"));
+Console.WriteLine($"\nFound {binObjFolders.Count()} folders to remove in {sourceFolder}.\n");
+foreach (var folder in binObjFolders)
+{
+    try
+    {
+        Directory.Delete(folder, true);
+        Console.WriteLine($"Removed {folder}.");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Error: {e.Message}");
+    }
+}
